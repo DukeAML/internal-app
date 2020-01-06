2387e4b2eb2945db51c84963520efa29ce2f0134
Mary Gooneratne <mmg53@duke.edu>
2020-01-06 10:02:14 -0800

    Removed pg package and commented out pg code in Home

--- expo-src/screens/HomeScreen.js
+++ expo-src/screens/HomeScreen.js
@@ -14,16 +14,16 @@ import { MonoText } from '../components/StyledText';
 
 
 export default function HomeScreen() {
-  var pg = require('pg');
-  var connectionString = "postgres://postgres:BronCavs#23@localhost/ip:5432/dvdrental";
-  var pgClient = new pg.Client(connectionString);
-  pgClient.connect();
-  var query = pgClient.query("SELECT actor_id from actor");
-  query.on("row", function(row,result){
+  //var pg = require('pg');
+  //var connectionString = "postgres://postgres:BronCavs#23@localhost/ip:5432/dvdrental";
+  //var pgClient = new pg.Client(connectionString);
+  //pgClient.connect();
+  //var query = pgClient.query("SELECT actor_id from actor");
+  //query.on("row", function(row,result)
 
-    result.addRow(row);
+    //result.addRow(row);
     
-    });
+    //});
   return (
     <View>
       <ScrollView>
